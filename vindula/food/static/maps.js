ff4ff7124988d45b11da2bdb641a106f00636b2e
denis <denis@liberiun-denis>
2011-11-29 12:11:30 -0200

    Update google maps scrips

--- vindula/food/static/maps.js
+++ vindula/food/static/maps.js
@@ -6,6 +6,8 @@ $j(document).ready(function(){
     var siberia = new google.maps.LatLng(60, 105);
     var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
     var browserSupportFlag =  new Boolean();
+    var marker = new Array(document.querySelectorAll("input.address").length);
+    var infowindow = new Array(document.querySelectorAll("input.address").length);
     
     function initialize() {
         geocoder = new google.maps.Geocoder();
@@ -15,24 +17,23 @@ $j(document).ready(function(){
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
         
-		
-		for(var c=1; c <= document.querySelectorAll("input.address").length; c++)
-		{
-			createMark(c)			
-		}
-	
-		function createMark(number) {
-            var marker;
+        for(var c=1; c <= document.querySelectorAll("input.address").length; c++)
+        {
+            createMark(c);
+        }
+    
+        function createMark(number) {
             var address = document.getElementById("address"+number).value;
-			var name = document.getElementById("name"+number).value;
-			var url = './restaurant';
-			var id = document.getElementById("id"+number).value;
+            var name = document.getElementById("name"+number).value;
+            var url = './restaurant';
+            var id = document.getElementById("id"+number).value;
+            
             geocoder.geocode( {'address': address}, function(results, status) {
                 
                 if (status == google.maps.GeocoderStatus.OK) 
                 {
                     var image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/restaurant.png';
-                    marker = new google.maps.Marker({
+                    marker[number-1] = new google.maps.Marker({
                         map: map,
                         position: results[0].geometry.location,
                         icon: image
@@ -43,22 +44,37 @@ $j(document).ready(function(){
                     alert("Geocode was not successful for the following reason: " + status);
                 }
                 
-                var infowindow = new google.maps.InfoWindow({
-                    content: '<div>'+					           
-		                        '<h2><a href="'+url+'?id='+id+'" target="_blank">'+name+'</a></h2>'+
-		                     '</div>'+
-		                     '<div>'+
-		                        address+
-		                     '</div>',
+                infowindow[number-1] = new google.maps.InfoWindow({
+                    content: '<div>'+                              
+                                '<h2><a href="'+url+'?id='+id+'" target="_blank">'+name+'</a></h2>'+
+                             '</div>'+
+                             '<div>'+
+                                address+
+                             '</div>',
 
                     size: new google.maps.Size(50,50)
                 });
                 
-                google.maps.event.addListener(marker, 'click', function() {
-                    infowindow.open(map, marker);
-                });                                
+                google.maps.event.addListener(marker[number-1], 'click', function() {
+                    infowindow[number-1].open(map, marker[number-1]);
+                });
+                
+                $j('.address-map'+number).click(function(){
+                    var pos = marker[number-1].getPosition();
+                    map.setCenter(pos);
+                    map.setZoom(15);
+                    for(var x=0; x < infowindow.length; x++)
+                    {
+                        if(x != number-1)
+                          infowindow[x].close();
+                    }
+                    google.maps.event.trigger(marker[number-1], 'click');
+                });
+                
             });
-		}
+            
+            
+        }
       
         // Try HTML5 geolocation
         if(navigator.geolocation) {
@@ -92,12 +108,10 @@ $j(document).ready(function(){
             browserSupportFlag = false;
             handleNoGeolocation(browserSupportFlag);
         }
+        
     } 
-	
-	function centraliza()
-	{
     
-	}
     
     google.maps.event.addDomListener(window, 'load', initialize);
+    
 });
--- vindula/food/static/maps_geocoder.js
+++ vindula/food/static/maps_geocoder.js
@@ -1,58 +1,63 @@
-var geocoder;
-var map;
-var marker;
+$j = jQuery.noConflict();
+$j(document).ready(function(){
 
-function initialize() 
-{
-	geocoder = new google.maps.Geocoder();
-	var latlng = new google.maps.LatLng(-23.548566,-46.64443);
-	var myOptions = 
-	{
-		zoom: 16,
-		center: latlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-}
+    var geocoder;
+    var map;
+    var marker;
+    
+    function initialize() 
+    {
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(-23.548566,-46.64443);
+        var myOptions = 
+        {
+            zoom: 16,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    }
+    
+    function codeAddress() {
+        var address = document.getElementById("address").value;
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            
+            if (status == google.maps.GeocoderStatus.OK) 
+            {
+                map.setCenter(results[0].geometry.location);
+                marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+            } 
+            
+            else 
+            {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+            
+            var infowindow = new google.maps.InfoWindow({
+                
+                content: '<div>' +
+                             '<h2>'+document.getElementById("name").value+'</h2>'+
+                         '</div>' +
+                         '<div>'+
+                           document.getElementById("address").value+
+                         '</div>',
+    
+                size: new google.maps.Size(100,100)
+            });
+            
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map, marker);
+            });
 
-function codeAddress() {
-	var address = document.getElementById("address").value;
-	geocoder.geocode( { 'address': address}, function(results, status) {
-		
-		if (status == google.maps.GeocoderStatus.OK) 
-		{
-			map.setCenter(results[0].geometry.location);
-			marker = new google.maps.Marker({
-				map: map,
-				position: results[0].geometry.location
-			});
-		} 
-		
-		else 
-		{
-		    alert("Geocode was not successful for the following reason: " + status);
-		}
-		
-		var infowindow = new google.maps.InfoWindow({
-			
-			content: '<div>' +
-			             '<h2>'+document.getElementById("name").value+'</h2>'+
-			         '</div>' +
-					 '<div>'+
-					   document.getElementById("address").value+
-					 '</div>',
+        });
+        
+        
+    }
+    
+    google.maps.event.addDomListener(window, 'load', initialize); 
+    google.maps.event.addDomListener(window, 'load', codeAddress);
 
-	        size: new google.maps.Size(100,100)
-	    });
-	    
-	    google.maps.event.addListener(marker, 'click', function() {
-	        infowindow.open(map, marker);
-	    });
-		
-	});
-	
-	
-}
-
-google.maps.event.addDomListener(window, 'load', initialize); 
-google.maps.event.addDomListener(window, 'load', codeAddress);
+});
